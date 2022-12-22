9cc286b769dace38d778974a1a144376b0a9c0a8
Penguin.jpg <sky026912@gmail.com>
2022-12-23 00:09:20 +0800

    add model and dataset file, add server and client code

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+
+HEADER = 10
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.26"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+while True:
+    try:
+        message = input()
+        send(message)
+    except KeyboardInterrupt:
+        send(DISCONNECT_MESSAGE)
+        print("disconnected")
+        break
